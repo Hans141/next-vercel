c3f5e25020a7a82d36ce8514edfddd375974d1ef
Hans <hans@techainer.com>
2021-10-28 15:11:20 +0700

    chore: check deploy vercel

--- pages/index.js
+++ pages/index.js
@@ -7,10 +7,10 @@ export default function Home() {
     <>
       <Head>
         <title>Ninja List | Home</title>
-        <meta name="keywords" content="ninjas"/>
+        <meta name="keywords" content="ninjas" />
       </Head>
       <div>
-        <h1 className={styles.title}>Homepage</h1>
+        <h1 className={styles.title}>Homepage with new commit</h1>
         <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
         <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
         <Link href="/ninjas/">
